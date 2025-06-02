c4b7720f3a2b264c0e91e4cfc20e6b1cdc5affd9
conwaymc <mike.conway@nih.gov>
2025-06-02 10:06:12 -0400

    fix failing test

--- tests/test_sanitize_json.py
+++ tests/test_sanitize_json.py
@@ -2,17 +2,20 @@ import json
 import unittest
 
 from accelerator_core.utils.json_sanitizer import JSONSanitizer
+from accelerator_core.utils.resource_utils import determine_test_resource_path
 
 
 class TestSanitizeJson(unittest.TestCase):
     def test_sanitize(self):
-        test_file = 'test_resources/sanitizeme.json'
+        test_file = determine_test_resource_path(
+            resource_name="sanitizeme.json", test_class="tests"
+        )
+
         with open(test_file) as json_data:
             raw_data = json_data.read()
             sanitized = JSONSanitizer.sanitize_json_string(raw_data)
             self.assertIsNotNone(sanitized["description"])
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
