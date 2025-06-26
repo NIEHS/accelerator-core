beae33ba3983bd280806467615eada37e800cf47
conwaymc <mike.conway@nih.gov>
2025-06-26 07:40:56 -0400

    serialize/deserialize

--- accelerator_core/utils/data_utils.py
+++ accelerator_core/utils/data_utils.py
@@ -2,6 +2,8 @@
 Various data handling utilities
 """
 
+import json
+
 
 def sanitize_boolean(val) -> bool:
 
@@ -15,3 +17,29 @@ def sanitize_boolean(val) -> bool:
             val = True
 
     return val
+
+
+def to_dict(obj):
+    if isinstance(obj, list):
+        return [to_dict(i) for i in obj]
+    elif hasattr(obj, "__dict__"):
+        return {k: to_dict(v) for k, v in obj.__dict__.items()}
+    else:
+        return obj
+
+
+def from_dict(cls, dict_obj):
+    obj = cls.__new__(cls)
+    for k, v in dict_obj.items():
+        if hasattr(obj, k):
+            setattr(obj, k, v)
+    return obj
+
+
+class Serializable:
+    def to_json(self):
+        return json.dumps(to_dict(self), indent=2)
+
+    @classmethod
+    def from_json(cls, json_str):
+        return from_dict(cls, json.loads(json_str))
